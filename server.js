800194e3ea8357866ac42090c3aae6115f8b21db
newalise <fogelman.alice@gmail.com>
2019-12-20 21:07:32 +0300

    change server path

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var jsonServer  = require('json-server');
 
 var server      = jsonServer.create();
-var router      = jsonServer.router(require('./db.js')());
+var router      = jsonServer.router(require('./src/server/db.js')());
 var middlewares = jsonServer.defaults();
 
 server.use(middlewares);
